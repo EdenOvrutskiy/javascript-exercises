edf0105e10a60d7a1e7d306a25bce17a68ad80fe
Eden Ovrtuskiy <none>
2022-06-16 13:32:24 -0400

    passed first 2 tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,13 @@
-const sumAll = function() {
+//TODO
+//enable large numbers - maybe bigInt can help?
+const sumAll = function (integer, integer2) {
+    //return the sum of integer +(integer + 1).. 
+    //                           +integer2)
+    let sum = 0;
+    for (i = integer; i <= integer2; ++i) {
+        sum += i;
+    }
+    return sum;
 
 };
 
