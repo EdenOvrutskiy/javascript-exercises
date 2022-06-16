f91ad4b0d8f08c45c0e82f2db75f8ffa13e8236f
Eden Ovrtuskiy <none>
2022-06-16 14:01:28 -0400

    passed tests 3, 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,12 +3,44 @@
 const sumAll = function (integer, integer2) {
     //return the sum of integer +(integer + 1).. 
     //                           +integer2)
-    let sum = 0;
-    for (i = integer; i <= integer2; ++i) {
-        sum += i;
-    }
-    return sum;
+    //despite integers including negative numbers,
+    //specs want to return an 'ERROR' string
+    //if encountered.
 
+    function isNegative(integer) {
+        return (integer < 0);
+    }
+
+    if (isNegative(integer)) {
+        return 'ERROR';
+    }
+    else if (isNegative(integer2)) {
+        return 'ERROR';
+    }
+    else {
+        //iterate on the sequence of numbers
+        //make sure the iteration happens in
+        //the right direction - i.e
+        //from the smaller number to the larger
+
+        //make one loop with variable "bigger int"
+
+        if (integer < integer2) {
+            smallerInt = integer;
+            biggerInt = integer2;
+        }
+        else {
+            smallerInt = integer2;
+            biggerInt = integer;
+        }
+
+        let sum = 0;
+        for (i = smallerInt; i <= biggerInt; ++i) {
+            sum += i;
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
