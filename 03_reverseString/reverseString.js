0d7e9deaa79eafde9908604fde53e07b3fae7c49
SpiderRider067 <SpiderRider067@gmail.com>
2022-06-16 11:42:31 -0400

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,30 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    //reversing a string
+    //could access the last letter with a negative index
+    //console.log(string.slice(-1));
 
+    //could access the length with a built-in method
+    //console.log(string.length);
+
+    //can target a character with index
+    //console.log(string[7]);
+
+    //a for (each x in y) loop?
+    //for (letter of*? string) {
+        //console.log(letter);
+    //}
+    
+    ///I think a countdown loop from last index is fine
+
+    let reversedString = '';
+    const lastLetterIndex = string.length - 1;
+    for (let i = lastLetterIndex; i >= 0; --i) {
+        reversedString += string[i];
+    }
+    //console.log(reversedString);
+    return reversedString;
 };
+//reverseString("helloSir");
 
 // Do not edit below this line
 module.exports = reverseString;
