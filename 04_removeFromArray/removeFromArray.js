30ae0df46a9bd541841d9734a9f46dc713a2de18
Eden Ovrtuskiy <none>
2022-06-17 13:38:32 -0400

    applied rest operator, refactored variable names, removed an if statement

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,13 @@
-const removeFromArray = function (array, removeThis) {
+const removeFromArray = function (array, ...removeThese) {
     //go over every value passed to the function
-    for (const argument of arguments) {
-        if (argument != argument[0]) {//except for the array
-            let removeThis = argument;
-            if (array.includes(removeThis)) { //this prevents
-                //the splice method from being passed -1 an breaking
-                //find the index of what should be removed 
-                const indexToRemove = array.indexOf(removeThis);
-                //use the splice method to remove an index from an array
-                const removeCount = 1; //to remove only a single item
-                array.splice(indexToRemove, removeCount);
-            }
+    for (const removeThis of removeThese) {
+        if (array.includes(removeThis)) { //this prevents
+            //the splice method from being passed -1 an breaking
+            //find the index of what should be removed 
+            const indexToRemove = array.indexOf(removeThis);
+            //use the splice method to remove an index from an array
+            const removeCount = 1; //to remove only a single item
+            array.splice(indexToRemove, removeCount);
         }
     }
     return array;
