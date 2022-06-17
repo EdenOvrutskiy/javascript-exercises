baf940827aef11fabe8c3741485f2482a325a3ac
Eden Ovrtuskiy <none>
2022-06-17 11:37:48 -0400

    solved exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    //check if divisible by 4 (1984, 2004)
+    if (year % 4 == 0) {
+        //if also(AND) divisible by 100, not a leap year
+        if (year % 100 == 0) {
+            //unless it's also(AND) divisible by 400
+            if (year % 400 == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+/*
+leap years:
+* not divisible by 100 (1800, 1900)
+  * unless divisible by 400 (2000, 1600)
+*/
