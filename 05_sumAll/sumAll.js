ae89c3941d8551102ef01d875d00d79aa1138893
Eden Ovrtuskiy <none>
2022-06-17 11:18:47 -0400

    split compound conditionals into multiple functions, removed all redundant comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,6 @@
-//TODO
-//is a concise if statement with many boolean operators
-//better than multiple if-else ?
-//  should I create a isInvalid(input) function for that block of 
-//  code?
-// try a switch statement for my if-else vs. boolean problem
 const sumAll = function (integer, integer2) {
     //return the sum of integer +(integer + 1).. 
     //                           +integer2)
-    //despite integers including negative numbers,
-    //specs want to return an 'ERROR' string
-    //if encountered.
-
     function isPositive(integer) {
         return (integer >= 0);
     }
@@ -19,97 +9,48 @@ const sumAll = function (integer, integer2) {
         return (typeof input == 'number');
     }
 
-    //too many boolean operators in an IF statement..
-    function isValidInput(integer, integer2) {
-        if (isPositive(integer)
-            && isPositive(integer2)
-            && isNumber(integer)
-            && isNumber(integer2)) {
-            return true;
-        }
+    function isValidInput(integer) { 
+        return (isPositive(integer) && isNumber(integer));
     }
-    function isBadInput(integer, integer2) {
-        if (isPositive(integer)) {
-        }
-        else if (isPositive(integer2)) {
-        }
-        else if (isNumber(integer)) {
-        }
-        else if (isNumber(integer2)) {
-        }
-        else {
-            return true;
-        }
-
+    
+    function areValidInput(integer, integer2) { 
+        return (isValidInput(integer) && isValidInput(integer2));
     }
-    //to use 
-    let isBadInput;
-    switch (true) {
-        case isPositive(integer):
-            isBadInput = false;
-        default:
-            isBadInput = true;
-            
+    
+    if (areValidInput(integer, integer2)) {
+        //create a sequence of numbers to add
+        let firstAddend;
+        let lastAddend;
+        //makes sure the iteration happens in
+        //the right direction - i.e
+        //from the smaller number to the larger
+        if (integer < integer2) {
+            firstAddend = integer;
+            lastAddend = integer2;
+        }
+        else if (integer > integer2) {
+            firstAddend = integer2;
+            lastAddend = integer;
+        }
+        else { //if they're equal, it doesn't matter which is first
+            firstAddend = integer2;
+            lastAddend = integer;
+        }
 
+        //create and add a sequence of numbers into a running total
+        let sum = 0;
+        for (let i = firstAddend; i <= lastAddend; ++i) {
+            sum += i;
+        }
+        return sum;
     }
+    else {
+        //despite integers including negative numbers,
+        //specs want to return an 'ERROR' string
+        //if encountered.
+        return 'ERROR';
+    }
+}; //end of sumAll function
 
-    //too much nesting
-    function isValidInput(integer, integer2) {
-        if (isPositive(integer)) {
-            if (isPositive(integer2)) {
-                if (isNumber(integer)) {
-                    if (isNumber(integer2)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        /*
-        //too many if / else-ifs + repetition with return..
-        if (isNegative(integer)) {
-            return 'ERROR';
-        }
-        else if (isNegative(integer2)) {
-            return 'ERROR';
-        }
-        else if (typeof integer != 'number') {
-            return 'ERROR';
-        }
-        else if (typeof integer2 != 'number') {
-            return 'ERROR';
-        }
-        */
-        if (isValidInput(integer, integer2)) {
-            //make sure the iteration happens in
-            //the right direction - i.e
-            //from the smaller number to the larger
-            let firstAddend;
-            let lastAddend;
-
-            if (integer < integer2) {
-                firstAddend = integer;
-                lastAddend = integer2;
-            }
-            else if (integer > integer2) {
-                firstAddend = integer2;
-                lastAddend = integer;
-            }
-            else { //if they're equal, it doesn't matter which is first
-                firstAddend = integer2;
-                lastAddend = integer;
-            }
-
-            //add each number to a running total
-            let sum = 0;
-            for (let i = firstAddend; i <= lastAddend; ++i) {
-                sum += i;
-            }
-            return sum;
-        }
-        else {
-            return 'ERROR';
-        }
-    };
-
-    // Do not edit below this line
-    module.exports = sumAll;
+// Do not edit below this line
+module.exports = sumAll;
